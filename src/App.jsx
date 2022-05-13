2a9ef45f7aa1efc0940dfae8dab2d6836ce31b77
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-05-14 06:59:17 +0900

    4-5 props の使い方

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { ColoredMessage } from './components/ColoredMessage';
+
+export const App = () => {
+  const onClickButton = () => {
+    alert();
+  }
+
+  return(
+    <>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+        <ColoredMessage color="blue" message="お元気ですか？"/>
+        <ColoredMessage color="pink" message="元気です！" />
+      <button onClick={onClickButton}>ボタン</button>
+    </>
+  )
+}
