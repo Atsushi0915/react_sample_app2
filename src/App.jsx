3c143068442443d93462a7959b0466ec57053ad8
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-05-15 06:19:31 +0900

    コンポーネントのメモ化(1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import { useState } from 'react'
-import { ColoredMessage } from './components/ColoredMessage';
+import { useState, memo } from "react";
+import { Child1 } from "./components/Child1"
+import { Child4 } from "./components/Child4"
 
+export const App = memo(() => {
+  console.log("Appレンダリング")
 
-export const App = () => {
   const [num, setNum] = useState(0);
 
   const onClickButton = () => {
     setNum(num + 1);
-  }
+  };
 
-  return(
+  return (
     <>
-      <h1 style={{ color: "red" }}>こんにちは！</h1>
-        <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
-        <ColoredMessage color="pink">元気です！</ColoredMessage>
       <button onClick={onClickButton}>ボタン</button>
       <p>{num}</p>
+      <Child1 />
+      <Child4 />
     </>
-  )
-}
+  );
+});
